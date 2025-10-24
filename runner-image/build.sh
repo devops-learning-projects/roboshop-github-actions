19d39ab2e0ea2eb3b4afccdf838218ac2e2a7133
Mainul Islam <mainulislam6168@gmail.com>
2025-10-24 08:40:45 +0600

    create runner image and kept in ecr

--- runner-image/build.sh
+++ runner-image/build.sh
@@ -1,2 +1,4 @@
-docker build -t rkalluru/github-runner .
-docker push rkalluru/github-runner
+git pull
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 804756348441.dkr.ecr.us-east-1.amazonaws.com
+docker build -t 804756348441.dkr.ecr.us-east-1.amazonaws.com/runner:latest .
+docker push 804756348441.dkr.ecr.us-east-1.amazonaws.com/runner:latest
